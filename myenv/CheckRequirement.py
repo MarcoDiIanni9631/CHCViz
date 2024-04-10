a1ea7f4c9fb035dc66c74107e9d4824d5e25b786
MarcoDiIanni9631 <marco.diianni.3@gmail.com>
2024-04-10 13:57:00 +0200

    Draft for Check preliminary requirement

---
+++ myenv/CheckRequirement.py
@@ -0,0 +1,39 @@
+import subprocess
+
+# Lista delle dipendenze
+required_packages = [
+    "solc",
+    "scala",
+  #  "logtalk",
+  # "graphviz"
+]
+
+# Funzione per controllare se una dipendenza è installata
+def check_package(package):
+    try:
+        subprocess.run([package, "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        return True
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.run([package, "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
+# Funzione per controllare tutte le dipendenze
+def check_requirements():
+    missing_packages = []
+    for package in required_packages:
+        if not check_package(package):
+            missing_packages.append(package)
+    return missing_packages
+
+# Esegui il controllo delle dipendenze
+if __name__ == "__main__":
+    missing_packages = check_requirements()
+    if missing_packages:
+        print("Le seguenti dipendenze sono mancanti:")
+        for package in missing_packages:
+            print(f"- {package}")
+    else:
+        print("Tutte le dipendenze sono soddisfatte.")
